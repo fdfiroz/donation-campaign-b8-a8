ef2ca0e58687a3fd4a29e98b55f4117391dee8ff
fdfiroz <firozahamed0@gmail.com>
2023-09-26 02:28:44 +0600

    setSearchQuery Complead

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
     <Banner handelSearch={handelSearch} handleTextChange={handleTextChange}></Banner>
   
     <div className="grid grid-cols-4 gap-6">
-    {
+    {filteredData.length > 0 ? filteredData?.map(donation => <HomeDonationCard key={donation.id} donation={donation}></HomeDonationCard>) :
         data?.map(donation => <HomeDonationCard key={donation.id} donation={donation}></HomeDonationCard>)
     }
     </div>
